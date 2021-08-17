ad5ab1b51d84f9587f21f2a795792ef0def22b22
Ícaro Costa <37416658+headmyst@users.noreply.github.com>
2021-08-17 14:35:09 -0300

    Done!
    Criada a função para tratar uma lista vazia.

--- lib/list_length.ex
+++ lib/list_length.ex
@@ -1,5 +1,10 @@
 defmodule ListLength do
-  def call() do
-    #TO DO
+  def call(list), do: amount(list, 0)
+
+  defp amount([], acc), do: acc
+
+  defp amount([head | tail], acc) do
+    acc = acc + 1
+    amount(tail, acc)
   end
 end
